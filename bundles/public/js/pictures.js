e24931c0d7d6122ca98248aadb7796d04495c8da
Roman Alyakritskiy <rawork@yandex.ru>
2015-12-15 02:33:25 +0300

    fix

--- bundles/public/js/pictures.js
+++ bundles/public/js/pictures.js
@@ -14,9 +14,15 @@
         var totalSlides = 0;
         var picturesObj = {};
         var picturesArray = [];
+        var popupSlick = $('.modal-pictures');
 
         var elementHtml = function(item, pos) {
             return '<div class="picture"><div class="img"><div class="picture-vote">Голосовать</div><img data-lazy="'+ item.picture_value.extra.main.path +'"></div><div class="person">'+item.person+ ', ' + item.age +'</div><div class="city">' + item.city + '</div><div class="name"><a href="" data-position="'+pos+'" data-id="'+item.id+'" data-category="'+item.age_id+'">'+item.name+'</a></div>'+(parseInt(item.position) > 0 ? '<div class="place">'+item.position+' место</div>' : '' )+'<div class="idea">Идея:<br>' + item.idea + '</div></div>';
+        };
+
+        var modalElementHtml = function (item) {
+            var text = '<a href="#" class="popup-prev"></a> <a href="#" class="popup-next"></a>';
+            return '<div class="modal-picture"><img src="'+item.picture_value.extra.big.path+'">'+(parseInt(item.place) > 0 ? '<div class="relative"><div class="place"><span>'+item.place+' место</span></div></div>' : '') + '<div class="title">'+item.name+'</div><div class="person">'+item.person+' ('+item.city+'), '+item.age+'</div><div class="idea">Идея:<br>'+item.idea+'</div></div>';
         };
 
         var buildGallery = function() {
@@ -130,27 +136,46 @@
 
         var setArrowVibibility = function(currentSlide, totalSlides) {
             if (currentSlide+1 >= totalSlides) {
-                $('.slick-next').hide();
+                $('.gallery-next').hide();
             } else {
-                $('.slick-next').show();
+                $('.gallery-next').show();
             }
 
             if (0 >= currentSlide) {
-                $('.slick-prev').hide();
+                $('.gallery-prev').hide();
             } else {
-                $('.slick-prev').show();
+                $('.gallery-prev').show();
             }
 
             if (1 == totalSlides) {
-                $('.slick-prev').hide();
-                $('.slick-next').hide();
+                $('.gallery-prev').hide();
+                $('.gallery-next').hide();
             }
-        }
+        };
+
+        var setModalArrowVibibility = function(currentSlide, totalSlides) {
+            if (currentSlide+1 >= totalSlides) {
+                $('.popup-next').hide();
+            } else {
+                $('.popup-next').show();
+            }
+
+            if (0 >= currentSlide) {
+                $('.popup-prev').hide();
+            } else {
+                $('.popup-prev').show();
+            }
+
+            if (1 == totalSlides) {
+                $('.popup-prev').hide();
+                $('.popup-next').hide();
+            }
+        };
 
         var initSliderPlugin = function(windowWidth) {
             if (windowWidth >= 900) {
-                var prevArrow = '<button type="button" class="slick-prev"><img src="/bundles/public/img/prev.png"></button>';
-                var nextArrow = '<button type="button" class="slick-next"><img src="/bundles/public/img/next.png"></button>';
+                var prevArrow = '<button type="button" class="slick-prev gallery-prev"><img src="/bundles/public/img/prev.png"></button>';
+                var nextArrow = '<button type="button" class="slick-next gallery-next"><img src="/bundles/public/img/next.png"></button>';
             } else {
                 var prevArrow = '';
                 var nextArrow = '';
@@ -167,7 +192,7 @@
             }).on('afterChange', function(event, slick, currentSlide){
                 setArrowVibibility(currentSlide, totalSlides);
                 $('#slide-current').html(currentSlide+1);
-            });;
+            });
 
             $('#slide-current').html(currentSlide);
             $('#slide-total').html(totalSlides);
@@ -193,7 +218,7 @@
             buildGallery();
         });
 
-        $(document).on('click', '.picture .name a, a.popup-prev, a.popup-next', function(e) {
+        $(document).on('click', '.picture .name a', function(e) {
             e.preventDefault();
 
             console.log('click picture a');
@@ -204,40 +229,55 @@
 
             var pos = parseInt($(this).attr('data-position'));
 
-            var picture = picturesArray[pos];
-
-            var img = new Image();
-            img.src = picture.picture_value.extra.big.path;
-
-            $('.modal-picture img').attr('src', picture.picture_value.extra.big.path);
-            $('.modal-picture .title').html(picture.name);
-            if (0 < parseInt(picture.position)) {
-                $('.modal-picture .place span').html(picture.position+' место').show();
-                $('.modal-picture .place').show();
-            } else {
-                $('.modal-picture .place').hide();
+            for (i in picturesArray) {
+                popupSlick.append(modalElementHtml(picturesArray[i]))
             }
 
-            $('.modal-picture .person').html(picture.person+'('+picture.city+'), '+picture.age);
-            $('.modal-picture .idea').html('Идея:<br>'+ picture.idea);
-            if (pos <= 0) {
-                $('.popup-prev').hide();
-            } else {
-                $('.popup-prev').show();
-            }
-            if (pos >= picturesArray.length-1) {
-                $('.popup-next').hide();
-            } else {
-                $('.popup-next').show();
-            }
+            popupSlick.slick({
+                infinite: false,
+                dots : false,
+                lazyLoad: 'progressive',
+                slidesToShow: 1,
+                initialSlide: pos,
+                adaptiveHeight: true,
+                prevArrow: '<button class="slick-prev popup-prev"></button>',
+                nextArrow: '<button class="slick-next popup-next"></button>'
+            }).on('afterChange', function(event, slick, currentSlide){
+                setModalArrowVibibility(currentSlide, picturesArray.length);
+            });
 
-            $('.popup-prev').attr('data-position', pos-1);
-            $('.popup-next').attr('data-position', pos+1);
+            //var picture = picturesArray[pos];
+
+            //var img = new Image();
+            //img.src = picture.picture_value.extra.big.path;
+            //
+            //$('.modal-picture img').attr('src', picture.picture_value.extra.big.path);
+            //$('.modal-picture .title').html(picture.name);
+            //if (0 < parseInt(picture.position)) {
+            //    $('.modal-picture .place span').html(picture.position+' место').show();
+            //    $('.modal-picture .place').show();
+            //} else {
+            //    $('.modal-picture .place').hide();
+            //}
+            //
+            //$('.modal-picture .person').html(picture.person+'('+picture.city+'), '+picture.age);
+            //$('.modal-picture .idea').html('Идея:<br>'+ picture.idea);
+            //if (pos <= 0) {
+            //    $('.popup-prev').hide();
+            //} else {
+            //    $('.popup-prev').show();
+            //}
+            //if (pos >= picturesArray.length-1) {
+            //    $('.popup-next').hide();
+            //} else {
+            //    $('.popup-next').show();
+            //}
+            //
+            //$('.popup-prev').attr('data-position', pos-1);
+            //$('.popup-next').attr('data-position', pos+1);
 
             $('body').addClass('modal-open');
-            $('.modal').show(0, function(){
-                setTimeout(function(){console.log($('.modal-dialog').position(), $('.modal-dialog').outerHeight(),  $('.popup-prev').position())}, 20);
-            });
+            $('.modal').show();
 
         });
 
