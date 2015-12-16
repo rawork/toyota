f4d4617f1a2f184b75cdc4f5b7790fe9ab02d77f
Roman Alyakritskiy <rawork@yandex.ru>
2015-12-17 00:22:48 +0300

    fixes

--- src/Fuga/PublicBundle/Controller/NewsController.php
+++ src/Fuga/PublicBundle/Controller/NewsController.php
@@ -47,8 +47,8 @@ class NewsController extends PublicController
 			throw $this->createNotFoundException('Событие '.$id.'  не найдено.');
 		}
 
-		$prev = $this->get('container')->getItem('news_news', 'date<"'.$news['date'].'"');
-		$next = $this->get('container')->getItem('news_news', 'date>"'.$news['date'].'"', 'date ASC');
+		$prev = $this->get('container')->getItem('news_news', 'date<"'.$news['date'].'"', 'date DESC');
+		$next = $this->get('container')->getItem('news_news', 'date>"'.$news['date'].'"');
 
 		return $this->render('news/detail.html.twig', compact('news', 'prev', 'next'));
 	}
