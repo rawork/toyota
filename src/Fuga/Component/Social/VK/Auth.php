492752b2c18d28723c99a6da97e3fe5830402f2d
Roman Alyakritskiy <rawork@yandex.ru>
2016-02-10 21:43:56 +0530

    gallery remark

--- src/Fuga/Component/Social/VK/Auth.php
+++ src/Fuga/Component/Social/VK/Auth.php
@@ -9,6 +9,58 @@
 namespace Fuga\Component\Social\VK;
 
 
-class Auth {
+class Auth
+{
+
+	private $container;
+	private $appId;
+	private $appSharedSecret;
+
+	public function __construct($container, $appId, $appSharedSecret)
+	{
+		$this->container = $container;
+		$this->appId = $appId;
+		$this->appSharedSecret = $appSharedSecret;
+	}
+
+	public function member()
+	{
+		$session = array();
+		$member = false;
+		$validKeys = array('expire', 'mid', 'secret', 'sid', 'sig');
+		$appCookie = $_COOKIE['vk_app_' . $this->appId];
+		if ($appCookie) {
+			$sessionData = explode('&', $appCookie, 10);
+			foreach ($sessionData as $pair) {
+				list($key, $value) = explode('=', $pair, 2);
+				if (empty($key) || empty($value) || !in_array($key, $validKeys)) {
+					continue;
+				}
+				$session[$key] = $value;
+			}
+			foreach ($validKeys as $key) {
+				if (!isset($session[$key])) return $member;
+			}
+			ksort($session);
+
+			$sign = '';
+			foreach ($session as $key => $value) {
+				if ($key != 'sig') {
+					$sign .= ($key . '=' . $value);
+				}
+			}
+			$sign .= $this->appSharedSecret;
+			$sign = md5($sign);
+			if ($session['sig'] == $sign && $session['expire'] > time()) {
+				$member = array(
+					'id' => intval($session['mid']),
+					'secret' => $session['secret'],
+					'sid' => $session['sid']
+				);
+			}
+		}
+
+		return $member;
+	}
 
 }
